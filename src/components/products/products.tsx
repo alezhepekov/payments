e57dcf2bf9fae1af3c0db9660ccc429daf5b2340
Alexey Lezhepekov <a.lezhepekov@yandex.ru>
2025-12-27 22:06:13 +0300

    Added basic layout schema

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -7,24 +7,37 @@ function Products() {
   const { width, containerRef, mounted } = useContainerWidth();
 
   const layout = [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-    { i: 'b', x: 1, y: 0, w: 3, h: 2 },
-    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+    { i: 'i11', x: 0, y: 0, w: 3, h: 2, static: true },
+    { i: 'i12', x: 3, y: 0, w: 3, h: 2, static: true },
+    { i: 'i13', x: 6, y: 0, w: 3, h: 2, static: true },
+    { i: 'i14', x: 9, y: 0, w: 3, h: 2, static: true },
+    { i: 'i21', x: 0, y: 2, w: 3, h: 2, static: true },
+    { i: 'i22', x: 3, y: 2, w: 3, h: 2, static: true },
+    { i: 'i23', x: 6, y: 2, w: 3, h: 2, static: true },
+    { i: 'i24', x: 9, y: 2, w: 3, h: 2, static: true },
   ];
 
   return (
-    <div ref={containerRef} style={{ width: '100%', height: '500px' }}>
-      {mounted && (
-        <ReactGridLayout
-          layout={layout}
-          width={width}
-        >
-          {}
-          <div key="a" className="product-card">a</div>
-          <div key="b" className="product-card">b</div>
-          <div key="c" className="product-card">c</div>
-        </ReactGridLayout>
-      )}
+    <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+      <div style={{ display: 'flex', width: '15%', height: '100%' }}>
+        Categories
+      </div>
+        <div ref={containerRef} style={{ display: 'flex', width: '85%', height: '100%' }}>
+          {mounted && (
+            <ReactGridLayout
+              layout={layout}
+              width={width}>
+              <div key="i11" className="product-card-container">i11</div>
+              <div key="i12" className="product-card-container">i12</div>
+              <div key="i13" className="product-card-container">i13</div>
+              <div key="i14" className="product-card-container">i14</div>
+              <div key="i21" className="product-card-container">i21</div>
+              <div key="i22" className="product-card-container">i22</div>
+              <div key="i23" className="product-card-container">i23</div>
+              <div key="i24" className="product-card-container">i24</div>
+            </ReactGridLayout>
+          )}
+        </div>
     </div>
   );
 }
